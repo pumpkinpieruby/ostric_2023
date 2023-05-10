2cbf0b153571124ff5113fe1978d0c37ed730aea
pumpkinpieruby <133138357+pumpkinpieruby@users.noreply.github.com>
2023-05-10 23:21:58 +0700

    Add files via upload

---
+++ tugas1.js
@@ -0,0 +1,26 @@
+//menampilkan angka 1 sampai 10
+console.log("angka 1-10");
+for(let i = 1; i <= 10; i++)
+{
+    console.log(i);
+}
+
+//menampilkan bilangan ganjil
+console.log("bilangan ganjil");
+for(let j = 1; j <= 10; j++)
+{
+    if(j % 2 == 1)
+    {
+        console.log(j);
+    }
+}
+
+//menampilkan bilangan genap
+console.log("bilangan genap");
+for(let k = 1; k <= 10; k++)
+{
+    if(k % 2 == 0)
+    {
+        console.log(k);
+    }
+}
